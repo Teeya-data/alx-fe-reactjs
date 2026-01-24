e1db5c74df2c2d8dc7a5d5d0201e2b1fbf57a63c
Teeya-data <tayamashoud19@gmail.com>
2026-01-24 13:11:17 -0800

    Add advanced search and filtering functionality

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,18 +2,33 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  
   addRecipe: (newRecipe) => set((state) => ({ 
     recipes: [...state.recipes, newRecipe] 
   })),
+  
   updateRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map((recipe) =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
+  
   deleteRecipe: (recipeId) => set((state) => ({
     recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
   })),
-  setRecipes: (recipes) => set({ recipes })
+  
+  setRecipes: (recipes) => set({ recipes }),
+  
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  
+  filterRecipes: () => set((state) => ({
+    filteredRecipes: state.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()) ||
+      recipe.description.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  }))
 }));
 
 export default useRecipeStore;
